e31e619c3c43f94470c709585f7da82c27c918e9
mcolombotto <mcolombotto@gmail.com>
2023-03-31 17:31:18 -0300

    installed dotenv, bcryptjs and jsonwebtoken dependencies

--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const url = "mongodb://127.0.0.1:27017/test";
 
+//mongoose.connect(process.env.URL);
 mongoose.connect(url);
 
 const connection = mongoose.connection;
