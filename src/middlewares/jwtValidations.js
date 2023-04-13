6f1b6f7a96f1e47cfbc3aa5d398f8f78de5a1e80
lucianodanert <lucianodanert@gmail.com>
2023-04-12 23:32:09 -0300

    fix  jwtValidations

--- src/middlewares/jwtValidations.js
+++ src/middlewares/jwtValidations.js
@@ -2,11 +2,17 @@ import jwt from "jsonwebtoken";
 
 const validateJWT = (req, res, next) => {
   
+
+
+  console.log(req.header("x-access-token"))
   const token = req.header("x-access-token");
+  console.log(token);
   if (!token) {
     res.status(401).json({ message: "A token in the request is required to be sent" });
     
+  }
     try {
+      console.log("Inicio de desencriptado")
       const payload = jwt.verify(token, process.env.SECRET_JWT);
       req.id = payload.uid;
       req.username = payload.username;
@@ -17,7 +23,7 @@ const validateJWT = (req, res, next) => {
         .json({ message: "Invalid token - authentification failed" });
     }
     next();
-  }
+  
 };
 
 export default validateJWT;
